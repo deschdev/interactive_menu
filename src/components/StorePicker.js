aa1e75748ce099a9a2b0bca2f2989a2a4589e63c
deschdev <94028614+deschdev@users.noreply.github.com>
2022-02-05 12:27:08 -0500

    error with event handling
    history.push is not allowing the application to transition smoothly back and forth

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { getFunName } from '../helpers';
 
+
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+	
+	goToStore = (event) => {
+		// 1) stopping the form from submitting
+		event.preventDefault();
+		// 2) getting the text from that input
+		const storeName = this.myInput.current.value;
+		// 3) change the page to /store/whatever-they-entered
+		this.props.history.push(`/store/${storeName}`);
+	};
+
 	render() {
 		return (
-			<React.Fragment>
-				<form className="store-selector">
+				<form className="store-selector" onSubmit={this.goToStore}>
 					<h2>Please Enter a Store</h2>
-					<input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
-					<button type="submit">Visit Store</button>
+					<input 
+						type="text"
+						ref={this.myInput} 
+						required 
+						placeholder="Store Name" 
+						defaultValue={getFunName()} 
+					/>
+					<button type="submit">Visit Store →</button>
 				</form>
-			</React.Fragment>
 		)
 	}
 }
