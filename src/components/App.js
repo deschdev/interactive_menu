463636c06b16f6ad67541fd4c75cd18f0792af87
deschdev <94028614+deschdev@users.noreply.github.com>
2022-02-05 16:16:22 -0500

    added fish state
    created the state for the fish form

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,31 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-  render() {
+  
+	state = {
+		fishes: {},
+		order: {}
+	};
+
+	addFish = (fish) => {
+		// 1) Take a copy of the existing state
+		const fishes = { ...this.state.fishes };
+		// 2) Add new fish to fishes variable
+		fishes[`fish${Date.now()}`] = fish;
+		// 3) Set the new fishes object to state
+		this.setState({
+			fishes
+		});
+	}
+	
+	render() {
     return (
       <div className="interactive-menu">
 				<div className="menu">
 					<Header tagline="Lunch Menu"/>
 				</div>
 					<Order />	
-					<Inventory />
+					<Inventory addFish={this.addFish}/>
 			</div>
     )
   }
