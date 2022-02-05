b42fe45d297d8653d0bb69e5982caf7242bc0b8d
deschdev <94028614+deschdev@users.noreply.github.com>
2022-02-05 16:48:07 -0500

    fish data has been added
    passed fish data to appear in app state.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   
@@ -19,6 +20,12 @@ class App extends React.Component {
 		this.setState({
 			fishes
 		});
+	};
+
+	loadSampleFishes = () => {
+		this.setState({
+			fishes: sampleFishes
+		})
 	}
 	
 	render() {
@@ -28,7 +35,7 @@ class App extends React.Component {
 					<Header tagline="Lunch Menu"/>
 				</div>
 					<Order />	
-					<Inventory addFish={this.addFish}/>
+					<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
 			</div>
     )
   }
