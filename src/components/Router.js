3053bb0473c5b5f281153e7797993d611def2524
deschdev <94028614+deschdev@users.noreply.github.com>
2022-02-05 14:43:10 -0500

    updated react-router-dom to 5.3.0
    had to go back to an old version of react-router-dom to fix my error.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import StorePicker from './StorePicker';
 import App from './App';
 import NotFound from './NotFound';
 
 const Router = () => (
     <BrowserRouter>
-			<Routes>
-				<Route exact={true} path="/" element={<StorePicker/>}/>
-				<Route path="/store/:storeId" element={<App/>}/>
-				<Route exact={true} path="*" element={<NotFound/>}/>
-			</Routes>
+			<Switch>
+				<Route exact path="/" component={StorePicker}/>
+				<Route path="/store/:storeId" component={App}/>
+				<Route component={NotFound}/>
+			</Switch>
     </BrowserRouter>  
 );
 
