53c1f9bfa2122551829843610b21f4f44535e1fa
deschdev <94028614+deschdev@users.noreply.github.com>
2022-02-05 22:45:35 -0500

    added state and props to order
    order has been styled - can add and remove menu items by making the state available or unavailable

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,15 @@ class App extends React.Component {
 			fishes: sampleFishes
 		})
 	}
+
+	addToOrder = (key) => {
+		// 1) Take a copy of state
+		const order = {...this.state.order}
+		// 2) Add or update to the order, or update the number in our order
+		order[key] = order[key] + 1 || 1;
+		// 3) Call setState to update our state object
+		this.setState({ order });
+	}
 	
 	render() {
     return (
@@ -35,11 +44,24 @@ class App extends React.Component {
 				<div className="menu">
 					<Header tagline="Lunch Menu"/>
 					<ul className="fishes">
-						{Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+						{Object.keys(this.state.fishes).map(key => (
+							<Fish 
+								key={key}
+								index={key} 
+								details={this.state.fishes[key]} 
+								addToOrder={this.addToOrder}
+							/>
+						))}
 					</ul>
 				</div>
-					<Order />	
-					<Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
+					<Order 
+						fishes={this.state.fishes} 
+						order={this.state.order}
+					/>	
+					<Inventory 
+						addFish={this.addFish} 
+						loadSampleFishes={this.loadSampleFishes}
+					/>
 			</div>
     )
   }
